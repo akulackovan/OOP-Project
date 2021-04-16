95cb30f5cbc45e06341e52e98c39b5c9619ac5e0
akulackovan <akulackovan@gmail.com>
2021-04-16 18:49:08 +0300

    Read service one from JSON

--- src/com/kulachkova/Main.java
+++ src/com/kulachkova/Main.java
@@ -2,15 +2,18 @@ package com.kulachkova;
 
 import com.kulachkova.ServiceSecond.JavaJson;
 import com.kulachkova.ServiceThree.Unloading;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException, ParseException {
         JavaJson jsonJava = new JavaJson();
-        Unloading unloading = new Unloading(jsonJava.runServiceOne(30));
-        jsonJava.write(unloading);
+        jsonJava.runServiceOne(30);
+        Unloading unloading = new Unloading(jsonJava.readServiceOne());
+        jsonJava.writeServiceThree(unloading);
         unloading.report();
     }
 }
--- src/com/kulachkova/ServiceSecond/JavaJson.java
+++ src/com/kulachkova/ServiceSecond/JavaJson.java
@@ -3,10 +3,15 @@ package com.kulachkova.ServiceSecond;
 import com.kulachkova.ServiceOne.Ship;
 import com.kulachkova.ServiceOne.ShipGenerator;
 import com.kulachkova.ServiceThree.Unloading;
-import org.json.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,10 +19,54 @@ public class JavaJson {
 
     private List<Ship> ships;
 
-    public List<Ship> runServiceOne (int number) {
+    public void runServiceOne (int number) {
         ShipGenerator ship = new ShipGenerator(number);
         ships = ship.getShips();
         write();
+        writeServiceOne();
+    }
+
+    public void writeServiceOne () {
+        JSONArray shipJSON = new JSONArray();
+        for (Ship ship : ships) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("Name ship", ship.getName_());
+            jsonObject.put("Arrival date", ship.getTimeBegin_().toString());
+            jsonObject.put("Departure date", ship.getTimeEnd_().toString());
+            jsonObject.put("Type of cargo", ship.getNameType());
+            jsonObject.put("Number of cargo", ship.getNumberOfCargo());
+            shipJSON.add(jsonObject);
+        }
+        JSONObject finalOutput = new JSONObject();
+        finalOutput.put("Ships", shipJSON);
+        try (FileWriter file = new FileWriter("src\\resource\\arrivalOfShips.JSON")) {
+            file.write(finalOutput.toString());
+            System.out.println("Successfully Copied JSON Object to File...");
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public List<Ship> readServiceOne () {
+        JSONParser parser = new JSONParser();
+        try {
+            Object  a =  parser.parse(new FileReader("src\\resource\\arrivalOfShips.JSON"));
+            JSONArray solutions = new JSONArray();
+            List<Ship> ships = new ArrayList<>();
+            Iterator iterator = solutions.iterator();
+            while (iterator.hasNext()) {
+                JSONObject ship = new JSONObject();
+                String name = (String) ship.get("Name ship");
+                Timestamp arrivalDate = Timestamp.valueOf((String) ship.get("Arrival date"));
+                String type = (String) ship.get("Type of cargo");
+                int number = (int) ship.get("Number of cargo");
+                ships.add(new Ship(name, arrivalDate, new Ship.Cargo(type, number)));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         return ships;
     }
 
@@ -35,29 +84,25 @@ public class JavaJson {
             String type = in.next();
             System.out.print("Cargo number: ");
             int number = in.nextInt();
-            if (number < 1)
-            {
+            if (number < 1) {
                 System.out.println("Number>0");
                 continue;
             }
             System.out.print("Day of begin: ");
             int day = in.nextInt();
-            if (day > 30 || day < 1)
-            {
+            if (day > 30 || day < 1) {
                 System.out.println("Day [1; 30]");
                 continue;
             }
             System.out.print("Hour of begin: ");
             int hour = in.nextInt();
-            if (hour > 23 || hour < 0)
-            {
+            if (hour > 23 || hour < 0) {
                 System.out.println("Day [0; 23]");
                 continue;
             }
             System.out.print("Minute of begin: ");
             int minute = in.nextInt();
-            if (minute > 60 || minute < 0)
-            {
+            if (minute > 60 || minute < 0) {
                 System.out.println("Minute [0; 59]");
                 continue;
             }
@@ -68,23 +113,8 @@ public class JavaJson {
         }
     }
 
-    public void write (Unloading unloading) {
+    public void writeServiceThree (Unloading unloading) {
         List<Ship> ships = unloading.getList();
-        JSONArray shipJSON = new JSONArray();
-        for (Ship ship : ships) {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("Name ship", ship.getName_());
-            jsonObject.put("Arrival date", ship.getTimeBegin_());
-            jsonObject.put("Departure date", ship.getTimeEnd_());
-            jsonObject.put("Type of cargo", ship.getNameType());
-            jsonObject.put("Number of cargo", ship.getNumberOfCargo());
-            jsonObject.put("Real arrival date", ship.getRealTimeArrival_());
-            jsonObject.put("Real time begin unloading", ship.getRealTimeBegin_());
-            jsonObject.put("Real time end unloading", ship.getRealTimeEnd_());
-            jsonObject.put("Wait time", ship.getWaitTime_());
-            jsonObject.put("Fine", ship.getFine_());
-            shipJSON.put(jsonObject);
-        }
         JSONObject report = new JSONObject();
         report.put("Fine", unloading.getFine());
         report.put("All Delay", unloading.getAllDelay());
@@ -94,15 +124,30 @@ public class JavaJson {
         report.put("Number Of Crane Loose", unloading.getNumberOfCraneLoose());
         report.put("Number Of Ships", unloading.getNumberOfShips());
         report.put("Time Wait", unloading.getTimeWait());
-        shipJSON.put(report);
-        try (FileWriter file = new FileWriter("src\\resource\\arrivalOfShips.JSON")) {
-            file.write(shipJSON.toString());
+        JSONArray shipJSON = new JSONArray();
+        for (Ship ship : ships) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("Name ship", ship.getName_());
+            jsonObject.put("Arrival date", ship.getTimeBegin_().toString());
+            jsonObject.put("Departure date", ship.getTimeEnd_().toString());
+            jsonObject.put("Type of cargo", ship.getNameType());
+            jsonObject.put("Number of cargo", ship.getNumberOfCargo());
+            jsonObject.put("Real arrival date", ship.getRealTimeArrival_().toString());
+            jsonObject.put("Real time begin unloading", ship.getRealTimeBegin_().toString());
+            jsonObject.put("Real time end unloading", ship.getRealTimeEnd_().toString());
+            jsonObject.put("Wait time", ship.getWaitString());
+            jsonObject.put("Fine", ship.getFine_());
+            shipJSON.add(jsonObject);
+        }
+        JSONObject finalOutput = new JSONObject();
+        finalOutput.put("Report", report);
+        finalOutput.put("Ships", shipJSON);
+        try (FileWriter file = new FileWriter("src\\resource\\report.JSON")) {
+            file.write(finalOutput.toString());
             System.out.println("Successfully Copied JSON Object to File...");
         }
         catch (Exception e) {
             System.out.println(e);
         }
     }
-
-
 }
